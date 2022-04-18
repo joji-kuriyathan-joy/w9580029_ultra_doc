cccb4038595b0c1e0871e0d3e3cdc1eb52739b36
joji-kuriyathan-joy <w9580029@live.tees,ac,uk>
2022-04-18 23:31:38 +0530

    Calender Designer

--- app/src/main/java/uk/ac/tees/aad/w9580029_ultra_doc/CreateDocModelActivity.java
+++ app/src/main/java/uk/ac/tees/aad/w9580029_ultra_doc/CreateDocModelActivity.java
@@ -1,13 +1,36 @@
 package uk.ac.tees.aad.w9580029_ultra_doc;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class CreateDocModelActivity extends AppCompatActivity {
+    @VisibleForTesting
+    public ProgressBar mProgressBarCD;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    public void setProgressBarCD(int resId) {
+        mProgressBarCD = findViewById(resId);
+    }
+
+    public void showProgressBarCD() {
+        if (mProgressBarCD != null) {
+            mProgressBarCD.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideProgressBarCD() {
+        if (mProgressBarCD != null) {
+            mProgressBarCD.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
 }
